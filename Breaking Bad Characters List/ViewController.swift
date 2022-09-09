a615a79c2af432ca4a1cce2f986a1e8d93bb4f40
Oleg Fedotov <trevor706@gmail.com>
2022-09-09 17:50:14 +0300

    Initial Commit

---
+++ Breaking Bad Characters List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Breaking Bad Characters List
+//
+//  Created by Olegio on 09.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
